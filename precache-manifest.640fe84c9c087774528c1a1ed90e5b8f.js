3a237451fc1a229967e8ebc93c2018f427b5f59e
Diego Soares <soares.diego95@hotmail.com>
2020-06-25 17:34:56 -0300

    Updates

---
+++ precache-manifest.640fe84c9c087774528c1a1ed90e5b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c298cc64aa33ee6c58b2d71334acefca",
+    "url": "/jogo-da-velha/index.html"
+  },
+  {
+    "revision": "ae71a306ba5847bce710",
+    "url": "/jogo-da-velha/static/css/main.0261bd76.chunk.css"
+  },
+  {
+    "revision": "b611b0475bce2aed4b63",
+    "url": "/jogo-da-velha/static/js/2.02fead5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jogo-da-velha/static/js/2.02fead5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae71a306ba5847bce710",
+    "url": "/jogo-da-velha/static/js/main.ca819b4c.chunk.js"
+  },
+  {
+    "revision": "880a8a925c8ddea7800d",
+    "url": "/jogo-da-velha/static/js/runtime-main.5147afd0.js"
+  }
+]);
